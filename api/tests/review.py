e30679e2bbd9dee2f5580095c3c83176387252d1
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-07-24 12:58:49 -0700

    Uncomment tests.

--- api/tests/review.py
+++ api/tests/review.py
@@ -62,94 +62,94 @@ class FlaskTestCase(unittest.TestCase):
             user=user_id
         ))
 
-    # def test_get_reviews(self):
-    #     res = self.app.get('/users/2/my_reviews')
-    #
-    #     '''The length of a user with no reviews should be 0.'''
-    #     self.assertEqual(len(json.loads(res.data)), 0)
-    #     self.create_review('/users/1/reviews', 'test', 2)
-    #
-    #     '''After creating a new review of user 2, the length should be 1.'''
-    #     res = self.app.get('/users/2/my_reviews')
-    #     self.assertEqual(len(json.loads(res.data)), 1)
-    #
-    #     '''Query for an unknown user id number should return 404 status.'''
-    #     res = self.app.get('/users/3/reviews')
-    #     self.assertEqual(res.status_code, 404)
+    def test_get_reviews(self):
+        res = self.app.get('/users/2/my_reviews')
 
-    # def test_post_reviews(self):
-    #     '''Query for an unknown user id number should return 404 status.'''
-    #     res = self.create_review('/users/3/reviews', 'test', 2)
-    #     self.assertEqual(res.status_code, 404)
-    #
-    #     '''Create the review corretly when all parameters are passed.'''
-    #     for i in range(1, 3):
-    #         res = self.create_review('/users/1/reviews', 'test', 2)
-    #         self.assertEqual(json.loads(res.data).get('touserid'), 1)
-    #         self.assertEqual(json.loads(res.data).get('fromuserid'), 2)
-    #         self.assertEqual(json.loads(res.data).get('id'), i)
-    #
-    #     '''Test for a cases where required parameters are missing or improper
-    #     types are passed as parameter (e.g., an string instead of an int).'''
-    #     missing_message = self.app.post('/users/1/reviews', data=dict(
-    #         user=2
-    #     ))
-    #     missing_user = self.app.post('/users/1/reviews', data=dict(
-    #         message="test"
-    #     ))
-    #     self.assertEqual(missing_message.status_code, 404)
-    #     self.assertEqual(missing_user.status_code, 404)
-    #
-    #     '''TODO: Add test for incorrect data types.'''
+        '''The length of a user with no reviews should be 0.'''
+        self.assertEqual(len(json.loads(res.data)), 0)
+        self.create_review('/users/1/reviews', 'test', 2)
 
-    # def test_list(self):
-    #     '''Create two reviews from user 2 to user 1.'''
-    #     for i in range(1, 3):
-    #         res = self.create_review('/users/1/reviews', 'test_' + str(i), 2)
-    #
-    #     '''This should return the review for the respective users.'''
-    #     res = self.app.get('/users/1/reviews/1')
-    #     self.assertEqual(json.loads(res.data)[0].get('message'), 'test_1')
-    #     res = self.app.get('/users/1/reviews/2')
-    #     self.assertEqual(json.loads(res.data)[0].get('message'), 'test_2')
-    #
-    #     '''Test cases where the user or review ids do not exist.'''
-    #     res = self.app.get('/users/3/reviews/1')
-    #     self.assertEqual(res.status_code, 404)
-    #     res = self.app.get('/users/1/reviews/3')
-    #     self.assertEqual(res.status_code, 404)
+        '''After creating a new review of user 2, the length should be 1.'''
+        res = self.app.get('/users/2/my_reviews')
+        self.assertEqual(len(json.loads(res.data)), 1)
 
-    # def test_delete(self):
-    #     '''Create two reviews from user 2 to user 1.'''
-    #     for i in range(1, 3):
-    #         res = self.create_review('/users/1/reviews', 'test_' + str(i), 2)
-    #
-    #     '''Before delete, there are two reviews for user 1.'''
-    #     res = self.app.get('/users/1/reviews')
-    #     self.assertEqual(len(json.loads(res.data)), 2)
-    #
-    #     '''Delete review with id 1. Returns 200 status code.'''
-    #     res = self.app.delete('/users/1/reviews/1')
-    #     self.assertEqual(res.status_code, 200)
-    #
-    #     '''After delete, there is 1 review for user 1.'''
-    #     res = self.app.get('/users/1/reviews')
-    #     self.assertEqual(len(json.loads(res.data)), 1)
-    #
-    #     '''Test cases where the user or review ids do not exist.'''
-    #     res = self.app.delete('/users/3/reviews/1')
-    #     self.assertEqual(res.status_code, 404)
-    #     res = self.app.delete('/users/1/reviews/3')
-    #     self.assertEqual(res.status_code, 404)
+        '''Query for an unknown user id number should return 404 status.'''
+        res = self.app.get('/users/3/reviews')
+        self.assertEqual(res.status_code, 404)
 
-    # def test_get_my_reviews(self):
-    #     '''User 1 should initially have 0 reviews and then 1.'''
-    #     res = self.app.get('/users/1/my_reviews')
-    #     self.assertEqual(len(json.loads(res.data)), 0)
-    #     self.create_review('/users/2/reviews', 'test', 1)
-    #
-    #     res = self.app.get('/users/1/my_reviews')
-    #     self.assertEqual(len(json.loads(res.data)), 1)
+    def test_post_reviews(self):
+        '''Query for an unknown user id number should return 404 status.'''
+        res = self.create_review('/users/3/reviews', 'test', 2)
+        self.assertEqual(res.status_code, 404)
+
+        '''Create the review corretly when all parameters are passed.'''
+        for i in range(1, 3):
+            res = self.create_review('/users/1/reviews', 'test', 2)
+            self.assertEqual(json.loads(res.data).get('touserid'), 1)
+            self.assertEqual(json.loads(res.data).get('fromuserid'), 2)
+            self.assertEqual(json.loads(res.data).get('id'), i)
+
+        '''Test for a cases where required parameters are missing or improper
+        types are passed as parameter (e.g., an string instead of an int).'''
+        missing_message = self.app.post('/users/1/reviews', data=dict(
+            user=2
+        ))
+        missing_user = self.app.post('/users/1/reviews', data=dict(
+            message="test"
+        ))
+        self.assertEqual(missing_message.status_code, 404)
+        self.assertEqual(missing_user.status_code, 404)
+
+        '''TODO: Add test for incorrect data types.'''
+
+    def test_list(self):
+        '''Create two reviews from user 2 to user 1.'''
+        for i in range(1, 3):
+            res = self.create_review('/users/1/reviews', 'test_' + str(i), 2)
+
+        '''This should return the review for the respective users.'''
+        res = self.app.get('/users/1/reviews/1')
+        self.assertEqual(json.loads(res.data)[0].get('message'), 'test_1')
+        res = self.app.get('/users/1/reviews/2')
+        self.assertEqual(json.loads(res.data)[0].get('message'), 'test_2')
+
+        '''Test cases where the user or review ids do not exist.'''
+        res = self.app.get('/users/3/reviews/1')
+        self.assertEqual(res.status_code, 404)
+        res = self.app.get('/users/1/reviews/3')
+        self.assertEqual(res.status_code, 404)
+
+    def test_delete(self):
+        '''Create two reviews from user 2 to user 1.'''
+        for i in range(1, 3):
+            res = self.create_review('/users/1/reviews', 'test_' + str(i), 2)
+
+        '''Before delete, there are two reviews for user 1.'''
+        res = self.app.get('/users/1/reviews')
+        self.assertEqual(len(json.loads(res.data)), 2)
+
+        '''Delete review with id 1. Returns 200 status code.'''
+        res = self.app.delete('/users/1/reviews/1')
+        self.assertEqual(res.status_code, 200)
+
+        '''After delete, there is 1 review for user 1.'''
+        res = self.app.get('/users/1/reviews')
+        self.assertEqual(len(json.loads(res.data)), 1)
+
+        '''Test cases where the user or review ids do not exist.'''
+        res = self.app.delete('/users/3/reviews/1')
+        self.assertEqual(res.status_code, 404)
+        res = self.app.delete('/users/1/reviews/3')
+        self.assertEqual(res.status_code, 404)
+
+    def test_get_my_reviews(self):
+        '''User 1 should initially have 0 reviews and then 1.'''
+        res = self.app.get('/users/1/my_reviews')
+        self.assertEqual(len(json.loads(res.data)), 0)
+        self.create_review('/users/2/reviews', 'test', 1)
+
+        res = self.app.get('/users/1/my_reviews')
+        self.assertEqual(len(json.loads(res.data)), 1)
 
     def test_place_reviews(self):
         '''No reviews for this place yet returns empty array. Test a place
