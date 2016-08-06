b9fada8be50653656c6eeaac7ee3325c8483bbac
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-05 21:34:57 -0700

    Add CORS handling, and test file. Add dependencies list file.

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask, request
 from flask_json import FlaskJSON
+from flask_cors import CORS, cross_origin
+import os
 
 app = Flask(__name__)
 json = FlaskJSON(app)
+
+env = os.environ.get("AIRBNB_ENV")
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+"""List of origins for cross-origin connection in production."""
+if env == "production":
+    cors = CORS(app, resources={r"/*": {"origins": ["https://127.0.0.1/",
+                                                    "http://158.69.92.186/"]}})
