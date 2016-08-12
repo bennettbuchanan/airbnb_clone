2e282f7091c6f58364b0d1eeea9ac0e0bf852ded
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-11 20:39:09 -0700

    Add fabfile for automated deployment. Update name to requirements.txt.

--- api/fabfile.py
+++ api/fabfile.py
@@ -1,26 +1,39 @@
 from fabric.api import *
 
-env.user = 'ubuntu'
-
-env.hosts = ['54.174.235.23']
-
-dist = "mywebapp"
+dist = "airbnb_clone"
 maintenance_file = "maintenance.trigger"
+env.roledefs = {
+    'deploy_bennett': ['ubuntu@54.174.235.23', 'admin@158.69.92.186'],
+    'deploy_john': ['ubuntu@54.174.245.13', 'admin@158.69.70.204']
+}
 
 
-def pack():
-    '''Clone airbnb clone repo and create a zipfile.'''
-    local('git clone https://github.com/bennettbuchanan/airbnb_clone && cd airbnb_clone')
-    local("tar --exclude='*.tar.gz' -cvzf %s.tar.gz ." % dist)
+@roles('deploy_bennett')
+def deploy_bennett():
+    '''Clone airbnb_clone repo and create a zipfile.'''
+    local('git clone https://github.com/bennettbuchanan/airbnb_clone')
+    local("cd airbnb_clone ; tar --exclude='*.tar.gz' -czf %s.tar.gz ." % dist)
 
-
-def deploy():
     '''Unpack the tarfile and copy into the site's root on the webserver.'''
-    put('%s.tar.gz' % dist, '/tmp/%s.tar.gz' % dist)
+    put('airbnb_clone/%s.tar.gz' % dist, '/tmp/%s.tar.gz' % dist)
     run('sudo mkdir /tmp/%s' % dist)
     run('sudo tar -xzf /tmp/%s.tar.gz -C /var/www/html' % dist)
     run('sudo rm -rf /tmp/%s /tmp/%s.tar.gz' % (dist, dist))
-    local('rm -rf mywebapp.tar.gz airbnb_clone')
+    local('sudo rm -rf mywebapp.tar.gz airbnb_clone')
+
+
+@roles('deploy_john')
+def deploy_john():
+    '''Clone airbnb_clone repo and create a zipfile.'''
+    local('git clone https://github.com/johndspence/airbnb_clone')
+    local("cd airbnb_clone ; tar --exclude='*.tar.gz' -czf %s.tar.gz ." % dist)
+
+    '''Unpack the tarfile and copy into the site's root on the webserver.'''
+    put('airbnb_clone/%s.tar.gz' % dist, '/tmp/%s.tar.gz' % dist)
+    run('sudo mkdir /tmp/%s' % dist)
+    run('sudo tar -xzf /tmp/%s.tar.gz -C /home/admin/airbnb_clone' % dist)
+    run('sudo rm -rf /tmp/%s /tmp/%s.tar.gz' % (dist, dist))
+    local('sudo rm -rf mywebapp.tar.gz airbnb_clone')
 
 
 def maintenance_on():
