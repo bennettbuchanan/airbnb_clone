c4aa54118c1a0b53cb2ad0889c1b721d53952c2d
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-21 23:17:36 -0700

    Add initial webpack files for development.

---
+++ web/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./js/app.js",
+    output: {
+        path: __dirname,
+        filename: "./static/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
